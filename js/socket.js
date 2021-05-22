27e96935a0cda6eeb2f449e20c3567918cf54d22
tilda <iamtheworst@programmer.net>
2021-05-22 18:16:42 -0500

    only show when playing

--- js/socket.js
+++ js/socket.js
@@ -3,6 +3,7 @@ let pp = document.getElementById('pp')
 let hundred = document.getElementById('hundred-count')
 let fifty = document.getElementById('fifty-count')
 let miss = document.getElementById('miss-count')
+let counter = document.getElementById('counter')
 
 socket.onopen = () => console.log('connected')
 socket.onclose = event => {
@@ -14,10 +15,17 @@ socket.onerror = error => console.log('wtf just happened:', error)
 socket.onmessage = event => {
     try {
         let data = JSON.parse(event.data), menu = data.menu, play = data.gameplay
-        pp.innerHTML = play.pp.current
-        hundred.innerHTML = play.hits[100]
-        fifty.innerHTML = play.hits[50]
-        miss.innerHTML = play.hits[0]
+        if (menu.state === 2) {
+            counter.classList.remove('animated-out')
+            counter.classList.add('animated-in')
+            pp.innerHTML = play.pp.current
+            hundred.innerHTML = play.hits[100]
+            fifty.innerHTML = play.hits[50]
+            miss.innerHTML = play.hits[0]
+        } else {
+            counter.classList.remove('animated-in')
+            counter.classList.add('animated-out')
+        }
     } catch (err) {
         console.log('WTF JSOI:KJmg;klfejgkldjsgk;ldfjkl:', error)
     }
